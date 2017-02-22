d3c1d15d478b4d85e5d38ec2cfee456abe6c451c
Damir Rakhmanov <drakhmanov@ARTGROUP.local>
2017-02-22 18:57:07 +0400

    refactoried templates

--- src/helpers/templates/lineTemplate.jsx
+++ src/helpers/templates/lineTemplate.jsx
@@ -1,5 +1,46 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import './lineTemplate.scss'
+
+const ActiveTime = (props) =>
+  <div
+    className="time"
+    style={{
+      width: props.changes.duration,
+    }}
+  >
+    {props.changes.value ?
+      <span>{props.changes.value}</span>
+      : <span>{props.changes.duration}</span>}
+
+    <input type="hidden" name="duration" defaultValue={props.changes.duration} />
+    <input type="hidden" name="value" defaultValue={props.changes.value} />
+    <input type="hidden" name="startTime" defaultValue={props.changes.startTime} />
+    <input type="hidden" name="endTime" defaultValue={props.changes.endTime} />
+    <input type="hidden" name="id" defaultValue={props.id} />
+  </div>
+
+ActiveTime.propTypes = {
+  changes: PropTypes.object,
+  id: PropTypes.number,
+}
+
+const GapTime = (props) =>
+  <div
+    className="gap"
+    style={{
+      width: props.width,
+    }}
+  />
+
+GapTime.propTypes = {
+  width: PropTypes.number,
+}
+
+const LineFormer = props => <div className="time-former">{props.children} </div>
+
+LineFormer.propTypes = {
+  children: PropTypes.node,
+}
 
 const setLineTemplate = (resultValves) => {
     // console.log('resultValves.resultchanges', resultValves.resultchanges)
@@ -8,45 +49,14 @@ const setLineTemplate = (resultValves) => {
   for (let i = 0; i < resultValves.resultchanges.length; i++) {
     // console.log(resultValves.resultchanges[i])
     lineTemplate.push(
-      <div
-        className="time-former"
-        key={i}
-      >
-        <div
-          className="time"
-          data-duration={resultValves.resultchanges[i].duration}
-          data-startTime={resultValves.resultchanges[i].startTime}
-          data-endTime={resultValves.resultchanges[i].endTime}
-          style={{
-            width: resultValves.resultchanges[i].duration,
-          }}
-        >
-          {resultValves.resultchanges[i].value ? (<div>
-            <span>{resultValves.resultchanges[i].value}</span>
-            <span className="pull-right">{i}</span>
-          </div>
-          ) :
-            (<div>
-              <span>{resultValves.resultchanges[i].duration}</span>
-              <span className="pull-right">{i}</span>
-            </div>
-          )
-         }
-          <input type="hidden" name="duration" defaultValue={resultValves.resultchanges[i].duration} />
-          <input type="hidden" name="value" defaultValue={resultValves.resultchanges[i].value} />
-          <input type="hidden" name="startTime" defaultValue={resultValves.resultchanges[i].startTime} />
-          <input type="hidden" name="endTime" defaultValue={resultValves.resultchanges[i].endTime} />
-          <input type="hidden" name="id" defaultValue={i} />
-        </div>
-        <div
-          className="gap"
-          style={{
-            width: resultValves.resultchanges[i + 1] ?
-            resultValves.resultchanges[i + 1].startTime - resultValves.resultchanges[i].endTime
-            : null,
-          }}
+      <LineFormer key={i}>
+        <ActiveTime changes={resultValves.resultchanges[i]} id={i} />
+        <GapTime
+          width={resultValves.resultchanges[i + 1] ?
+                 resultValves.resultchanges[i + 1].startTime -
+                 resultValves.resultchanges[i].endTime : null}
         />
-      </div>
+      </LineFormer>
     )
   }
   return lineTemplate
